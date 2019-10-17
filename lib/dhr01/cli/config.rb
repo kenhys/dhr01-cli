f60ed751657143d41ad8ae916590ccbfe3743779
Kentaro Hayashi <kenhys@gmail.com>
2019-10-17 22:40:33 +0900

    Add login attributes

--- lib/dhr01/cli/config.rb
+++ lib/dhr01/cli/config.rb
@@ -2,6 +2,8 @@ module Dhr01
   module Cli
     class Config
       attr :router_ip, true
+      attr :login_name, true
+      attr :login_password, true
       def initialize(options={})
         @router_ip = "192.168.10.1"
       end
